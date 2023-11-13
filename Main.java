a50628b36979225ad07a4aafad109edf15e37634
Anastasia1921 <150111215+Anastasia1921@users.noreply.github.com>
2023-11-13 11:16:39 +0300

    Add files via upload

--- Main.java
+++ Main.java
@@ -3,6 +3,6 @@ package com.company;
 public class Main {
 
     public static void main(String[] args) {
-        new StudentWithTimerAndCalendar(new StudentWithTimer(new Student())).learn();
+        new StudentWithTimer(new Student()).learn();
     }
 }
